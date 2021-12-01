4e337b67e22c4987664d27c3aff8526342594ca3
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2021-11-30 23:44:10 -0500

    Delete percent.js

--- percent.js
+++
@@ -1,8 +0,0 @@
-const urlEncode = function(text) {
-  const regex = /\s+/g;
-  return text.replace(regex, '%20');
-};
-
-console.log(urlEncode("Lighthouse Labs"));
-console.log(urlEncode(" Lighthouse Labs "));
-console.log(urlEncode("blue is greener than purple for sure"));
